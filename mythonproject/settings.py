6e2bdeb68e04723ff3a059374304a4cd02638dab
Sainez Amon <sainezamon@gmail.com>
2019-07-06 02:52:58 +0300

    wht

--- mythonproject/settings.py
+++ mythonproject/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+WHITENOISE_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
